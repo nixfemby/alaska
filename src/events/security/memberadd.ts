9a53b3cf9e96e7def39b6406f23a1636d423732b
ruby <ayowxifu@tutanota.com>
2023-03-04 22:50:12 +0100

    ✨ feature: auto kick 2 weeks no pfp

---
+++ src/events/security/memberadd.ts
@@ -0,0 +1,34 @@
+import { ActivityType, Client, Embed, EmbedBuilder, GuildMember } from "discord.js";
+import { logger } from "../../helpers/logging";
+import { BotEvent } from "../../types";
+
+const event: BotEvent = {
+    name: "guildMemberAdd",
+    once: true,
+    execute : async (member: GuildMember, client: Client) => {
+        // Check account age;
+
+        if(Date.now() - member.user.createdTimestamp < 1000*60*60*24*14 ) {
+            if(!member.kickable) return logger.error('owo I cant kick them :(')
+            try {
+                member.send({ embeds: [new EmbedBuilder().setTitle(`You've been kicked from ${member.guild.name}`).setDescription("Reason: `account age below 14 days`").setColor("#ff9fbc").setTimestamp(Date.now())]})
+            } catch(e) {
+                logger.error("Couldnt dm them :(")
+                member.kick("automod | account age below 14 days")
+            }
+        }
+
+        // No profile picture
+        if(!member.avatarURL) {
+            if(!member.kickable) return logger.error('owo I cant kick them :(')
+            try {
+                member.send({ embeds: [new EmbedBuilder().setTitle(`You've been kicked from ${member.guild.name}`).setDescription("Reason: `no profile picture set`").setColor("#ff9fbc").setTimestamp(Date.now())]})
+            } catch(e) {
+                logger.error("Couldnt dm them :(")
+                member.kick("automod | no profile picture set")
+            }
+        }
+    }
+}
+
+export default event;
