04ce9f77e8a1a871d067bb582d10623bd20acb75
Ruby 3 <ayowxifu@tutanota.com>
2023-03-10 21:59:18 +0100

    ✨ feature: minimize filter

--- src/events/security/memberadd.ts
+++ src/events/security/memberadd.ts
@@ -9,26 +9,16 @@ const event: BotEvent = {
 
         if(Date.now() - member.user.createdTimestamp < 1000*60*60*24*14 ) {
             if(!member.kickable) return logger.error('owo I cant kick them :(')
-            try {
-                member.send({ embeds: [new EmbedBuilder().setTitle(`You've been kicked from ${member.guild.name}`).setDescription("Reason: `account age below 14 days`").setColor("#ff9fbc").setTimestamp(Date.now())]})
-            } catch(e) {
-                logger.error("Couldnt dm them :(")
-            }
-
-            member.kick("automod | account age below 14 days")
+            return member.kick("automod | account age below 14 days")
         }
 
         // No profile picture
         if(!member.avatarURL) {
             if(!member.kickable) return logger.error('owo I cant kick them :(')
-            try {
-                member.send({ embeds: [new EmbedBuilder().setTitle(`You've been kicked from ${member.guild.name}`).setDescription("Reason: `no profile picture set`").setColor("#ff9fbc").setTimestamp(Date.now())]})
-            } catch(e) {
-                logger.error("Couldnt dm them :(")
-            }
-            
-            member.kick("automod | no profile picture set")
+            return member.kick("automod | no profile picture set")
         }
+
+        return;
     }
 }
 
